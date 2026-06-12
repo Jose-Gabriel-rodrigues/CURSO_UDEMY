ff7d4cb32c6b33bee8838b5d7107dc5e4dd59cd2
gabriel <boagabriel63@gmail.com>
2026-06-12 01:35:02 -0300

    Desenvolvimento da API REST - Parte 1

---
+++ js/api_rest/eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'no-console': 'off',
+      'class-methods-use-this': 'off',
+      'import/first': 'off',
+      'no-param-reassign': 'off',
+      'no-shadow': 'off',
+    },
+  },
+]);
